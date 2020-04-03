edf4c4374ef1e91dc57b2eace8c8df2527ad708b
Yohannes Libanos <4yohannes@gmail.com>
2020-04-03 19:39:25 +0100

    Update TotalBarChart with additional options to support pyramid variant

--- components/TotalBarChart.vue
+++ components/TotalBarChart.vue
@@ -1,18 +1,44 @@
-
 <template>
   <v-card flat class="pr-1">
     <v-card-title>{{ title }}</v-card-title>
-    <apexchart type="bar" width="100%" :height="height" :series="series" :options="chartOptions" />
+    <apexchart
+      type="bar"
+      width="100%"
+      :height="height"
+      :series="series"
+      :options="chartOptions"
+    />
   </v-card>
 </template>
 <script>
-
 export default {
-  props: ["title", "height", "series", "labels", "horizontal", "showDataLabel"],
+  props: [
+    "title",
+    "height",
+    "series",
+    "grid",
+    "tooltip",
+    "colors",
+    "labels",
+    "labelFormatter",
+    "stacked",
+    "horizontal",
+    "showDataLabel"
+  ],
   data() {
+    const colors = this.$props.colors;
+    const grid = this.$props.grid;
+    const tooltip = this.$props.tooltip;
+    const labelFormatter = this.$props.labelFormatter;
+
     return {
       height: 300,
       chartOptions: {
+        chart: {
+          type: "bar",
+          height: 300,
+          stacked: this.$props.stacked
+        },
         plotOptions: {
           bar: {
             horizontal: this.$props.horizontal,
@@ -26,9 +52,18 @@ export default {
           textAnchor: "middle"
         },
         xaxis: {
-          categories: this.$props.labels
+          categories: this.$props.labels,
+          ...(labelFormatter
+            ? {
+                labels: {
+                  formatter: labelFormatter
+                }
+              }
+            : {})
         },
-       
+        ...(colors ? { colors } : {}),
+        ...(grid ? { grid } : {}),
+        ...(tooltip ? { tooltip } : {})
       }
     };
   },
